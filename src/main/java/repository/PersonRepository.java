1641a5fa3167b7517a92867d08842ee4bb3f9e1b
Juan scabuzzo <Juanscabu@gmail.com>
2020-10-17 17:58:05 -0300

    agrego proyecto

---
+++ src/main/java/repository/PersonRepository.java
@@ -0,0 +1,18 @@
+package repository;
+
+
+
+import model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    @Query("SELECT t FROM Person t where t.surname = :surname")
+    public List<Person> findAllBySurname(String surname);
+
+    @Query("SELECT t FROM Person t where t.name = :name")
+    public List<Person> findAllByName(String name);
+}
